b6e17f984a9d8e6f21fd99e9272f59a14295b7ab
nmadd <nate@fratmusic.com>
2016-01-13 18:58:02 -0500

    Added basic carousel

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -4,6 +4,7 @@ app.config(function ($stateProvider) {
         templateUrl: 'js/home/home.html',
         controller: function ($scope, ProductFactory, allProducts) {
         	$scope.products = allProducts;
+            $scope.slides = [{text: 'test', image: 'http://cdn.buzznet.com/assets/users16/pattygopez/default/justin-timberlake-britney-spears--large-msg-131118867529.jpg'}, {text: 'test2', image: 'http://toysfromthe90s.com/wp-content/uploads/2015/03/pogs.jpg'}];
         },
         resolve: {
         	allProducts: function(ProductFactory){
